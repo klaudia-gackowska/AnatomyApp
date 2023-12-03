344e550053b402ab16d6371bacbf36d4a7bbd698
klaudia-gackowska <308110@stud.umk.pl>
2023-12-03 11:50:52 +0100

    test

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 import * as THREE from 'three';
 
-const scene = new THREE.Scene();
+const arScene = new ARObject({
+  trackingMethod: 'best', // Metoda śledzenia
+  sourceType: 'webcam',   // Typ źródła (kamera)
+});
+
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebXRRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0xffc0cb } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+arScene.add( cube );
 
 camera.position.z = 5;
 
@@ -20,7 +24,7 @@ function animate() {
 	cube.rotation.x += 0.008;
 	cube.rotation.y += 0.008;
 
-	renderer.render( scene, camera );
+	renderer.render( arScene, camera );
 }
 
 animate();
