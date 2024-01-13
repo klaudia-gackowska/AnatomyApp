1d54daf4550730d271ef9a6fe38d65850c89010f
klaudia-gackowska <308110@stud.umk.pl>
2024-01-13 22:03:50 +0100

    split html o html file and js file

--- main.js
+++ main.js
@@ -1,3 +1,101 @@
+var scene, camera, renderer, clock, deltaTime, totalTime;
+var arToolkitSource, arToolkitContext;
+var markerRoot1, markerRoot2;
+var mesh1;
+
+initialize();
+animate();
+
+function initialize() {
+    scene = new THREE.Scene();
+
+    let ambientLight = new THREE.AmbientLight(0xcccccc, 0.5);
+    scene.add(ambientLight);
+
+    camera = new THREE.Camera();
+    scene.add(camera);
+
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+    });
+    renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+    renderer.setSize(640, 480);
+    renderer.domElement.style.position = 'absolute'
+    renderer.domElement.style.top = '0px'
+    renderer.domElement.style.left = '0px'
+    document.body.appendChild(renderer.domElement);
+
+    clock = new THREE.Clock();
+    deltaTime = 0;
+    totalTime = 0;
+
+    arToolkitSource = new THREEx.ArToolkitSource({
+        sourceType: 'webcam',
+    });
+
+    function onResize() {
+        arToolkitSource.onResize()
+        arToolkitSource.copySizeTo(renderer.domElement)
+        if (arToolkitContext.arController !== null) {
+            arToolkitSource.copySizeTo(arToolkitContext.arController.canvas)
+        }
+    }
+
+    arToolkitSource.init(function onReady() {
+        onResize()
+    });
+
+    window.addEventListener('resize', function () {
+        onResize()
+    });
+    arToolkitContext = new THREEx.ArToolkitContext({
+        cameraParametersUrl: 'data/camera_para.dat',
+        detectionMode: 'mono'
+    });
+    arToolkitContext.init(function onCompleted() {
+        camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
+    });
+
+    markerRoot1 = new THREE.Group();
+    scene.add(markerRoot1);
+    let markerControls1 = new THREEx.ArMarkerControls(arToolkitContext, markerRoot1, {
+        type: 'pattern',
+        patternUrl: "data/kg.patt",
+    })
+
+    let geometry1 = new THREE.CubeGeometry(1, 1, 1);
+    let material1 = new THREE.MeshNormalMaterial({
+        transparent: true,
+        opacity: 0.5,
+        side: THREE.DoubleSide
+    });
+
+    mesh1 = new THREE.Mesh(geometry1, material1);
+    mesh1.position.y = 0.5;
+
+    markerRoot1.add(mesh1);
+}
+
+function update() {
+    if (arToolkitSource.ready !== false)
+        arToolkitContext.update(arToolkitSource.domElement);
+}
+
+function render() {
+    renderer.render(scene, camera);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    deltaTime = clock.getDelta();
+    totalTime += deltaTime;
+    update();
+    render();
+}
+
+
+
 /* 
 import "./style.css";
 import * as THREE from 'three';
